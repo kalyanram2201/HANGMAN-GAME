f399cd6b5d1a047eec91d1c9ad31a37d49f9eba7
kalyanram2201 <kalyanramakula13@gmail.com>
2024-07-12 12:33:00 +0530

    Add files via upload

---
+++ word.py
@@ -0,0 +1,22 @@
+clues=[
+    'eat me keep doctor away',
+    'have 4 wheel',
+    'write with me',
+    'write in me',
+    'iam adictive',
+    'sleep on me',
+    'oppen me to enter',
+    'i will bitw you',
+    'iam used for water'
+]
+words=[
+    "apple",
+    "car",
+    "pen",
+    "book",
+    "phone",
+    "bed",
+    "door",
+    "dog",
+    "bottle"
+]
